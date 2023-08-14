8024d30923ce3a52a0a47501b32dd8eeb6420843
KAKA <zb_ios@sina.com>
2023-08-14 19:48:11 +0800

    添加一个Button

--- WH/WH/ViewController.swift
+++ WH/WH/ViewController.swift
@@ -11,6 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let button = UIButton.init(type: .custom)
+        button.frame = CGRect(x: 0, y: 100, width: 100, height: 40)
+        button.backgroundColor = UIColor.blue
+        button.setTitle("hello world", for: .normal)
+        view.addSubview(button)
         // Do any additional setup after loading the view.
     }
 
